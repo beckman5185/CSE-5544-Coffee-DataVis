707904e0cbd4b6247be691cd0c8cfd63cf99e140
beckman5185 <abeckman4002@gmail.com>
2025-03-26 16:26:41 -0400

    Cleaned up and commented code

--- market-analysis-demographic/app.py
+++ market-analysis-demographic/app.py
@@ -2,25 +2,31 @@
 from shiny.express import input, render, ui
 from shinywidgets import render_plotly
 import pandas as pd
-from pathlib import Path
-coffeeSurvey= pd.read_csv(Path(__file__).parent / "coffee_survey.csv")
 
-ui.page_opts(title="Market Analysis Interactive Visualizations", fillable=True)
+#read in data
+coffeeSurvey= pd.read_csv("CSE-5544-Coffee-DataVis\coffee_survey.csv")
 
+#page title
+ui.page_opts(title="Market Analysis - Customer Demographics", fillable=False)
+
+#select demographic variable
 with ui.sidebar():
     ui.input_selectize(
         "demographic", "Select demographic variable",
         ['age', 'gender', 'education_level', 'ethnicity_race', 'employment_status', 'number_children', 'political_affiliation']
     )
 
+#proportion heatmap
 with ui.card():
     ui.card_header("Proportion")
     @render_plotly
     def proportionHeatmap():
         import plotly.express as px
         
+        #get non-null preference data
         preferenceData = coffeeSurvey[coffeeSurvey['prefer_overall'].notnull()]
 
+        #group by level of demographic variable
         demographicGroups = preferenceData.groupby(input.demographic())['prefer_overall'].value_counts(normalize=True)
         demographicGroups = demographicGroups.reset_index()
         
@@ -29,18 +35,24 @@ with ui.card():
         #overallPreference[input.demographic()] = 'total'
         
         #demographicGroups = pd.concat([demographicGroups, overallPreference.reset_index()])
+
+        #pivot table to heatmap format
         demographicGroups = pd.pivot_table(demographicGroups, values='proportion', index=input.demographic(), columns='prefer_overall')
 
+        #make heatmap
         return px.imshow(demographicGroups, color_continuous_scale="mint")
 
+#count heatmap
 with ui.card():
     ui.card_header("Count")
     @render_plotly
     def countHeatmap():
         import plotly.express as px
         
+        #get non-null preference data
         preferenceData = coffeeSurvey[coffeeSurvey['prefer_overall'].notnull()]
 
+        #group by level of demographic variable
         demographicGroups = preferenceData.groupby(input.demographic())['prefer_overall'].value_counts(normalize=False)
         demographicGroups = demographicGroups.reset_index()
         
@@ -49,6 +61,9 @@ with ui.card():
         #overallPreference[input.demographic()] = 'total'
         
         #demographicGroups = pd.concat([demographicGroups, overallPreference.reset_index()])
+
+        #pivot table to heatmap format
         demographicGroups = pd.pivot_table(demographicGroups, values='count', index=input.demographic(), columns='prefer_overall')
 
+        #make heatmap
         return px.imshow(demographicGroups, color_continuous_scale="mint")
--- market-analysis-trait/app.py
+++ market-analysis-trait/app.py
@@ -2,61 +2,64 @@
 from shiny.express import input, render, ui
 from shinywidgets import render_plotly
 import pandas as pd
-from pathlib import Path
-coffeeSurvey= pd.read_csv(Path(__file__).parent / "coffee_survey.csv")
 
-ui.page_opts(title="Market Analysis Interactive Visualizations", fillable=True)
+#read in data
+coffeeSurvey= pd.read_csv("CSE-5544-Coffee-DataVis\coffee_survey.csv")
+
+#page title
+ui.page_opts(title="Market Analysis - Coffee Traits", fillable=True)
 
 with ui.sidebar():
+    #select coffee trait
     ui.input_selectize(
         "trait", "Select coffee trait",
         ['personal_preference', 'bitterness', 'acidity']
     )
 
+    #select coffees of interest
     ui.input_checkbox_group(
         "coffee", "Select coffee blend", 
         ['coffee_a', 'coffee_b', 'coffee_c', 'coffee_d']   
     )
 
 with ui.card():
-    ui.card_header("Count")
+    ui.card_header("Coffee Traits")
     @render_plotly
-    def dotChart():
+    def lineChart():
         import plotly.express as px
         import statistics
 
-    
+        #get coffees and traits chosen
         coffeeList = input.coffee()
         traitList = []
-
         if(len(coffeeList) > 0):
             traitList = [coffee + "_" + input.trait() for coffee in coffeeList]
 
-
-        preferenceData = coffeeSurvey[coffeeSurvey[traitList].notnull().all(axis=1)]
-        
+        #get non-null data from survey
+        ratingData = coffeeSurvey[coffeeSurvey[traitList].notnull().all(axis=1)]
         traitFrame = pd.DataFrame()
 
-
+        #for each rating column
         for i in range(len(traitList)):
-
-            traitData = preferenceData[traitList[i]].value_counts(normalize=False)
+            #get counts for each response
+            traitData = ratingData[traitList[i]].value_counts(normalize=False)
             
-            
+            #adjust into long format
             traitData = traitData.reset_index()
             traitData["coffee"] = traitList[i][0:8]
-
             traitData = traitData.rename(columns={traitList[i]: input.trait()})
 
+            #add to dataframe
             traitFrame = pd.concat([traitFrame, traitData])
 
 
-
         if not (traitFrame.empty):
+            #sort x values and colors
             traitFrame = traitFrame.sort_values(by=[input.trait(), "coffee"])
-            fig = px.line(traitFrame, y="count", x=input.trait(), color="coffee", markers=True, color_discrete_sequence=px.colors.qualitative.Set1) 
 
-            return fig
+            #make line chart
+            return px.line(traitFrame, y="count", x=input.trait(), color="coffee", markers=True, color_discrete_sequence=px.colors.qualitative.Set1) 
+
 
 
 
